73a041f514c18faac95c873671755f27cb4d334f
Aleksandre Gevorkyan <gevorkyanaleko@gmail.com>
2022-07-04 21:33:58 +0400

    feat: add selecting player state and UI

--- src/components/PlayerListItem/index.tsx
+++ src/components/PlayerListItem/index.tsx
@@ -1,17 +1,43 @@
-import {View, Text, Image} from 'react-native';
-import React from 'react';
+import {View, Text, Image, Pressable} from 'react-native';
+import React, {memo, useCallback} from 'react';
+import {useRecoilState} from 'recoil';
 
+//@ts-ignore
+import {myTeamState} from 'atoms/MyTeam';
 import {Player} from 'types';
 
 import styles from './styles';
+import {COLORS} from 'constants/Colors';
 
 type Props = {
   player: Player;
 };
 
 const PlayerListItem = ({player}: Props) => {
+  const [myPlayers, setMyPlayers] = useRecoilState<Player[]>(myTeamState);
+
+  const handlePlayerPress = useCallback(
+    (player: Player) => {
+      if (myPlayers.some(pl => pl.id === player.id)) {
+        return setMyPlayers(currentPlayer =>
+          currentPlayer.filter(pl => pl.id !== player.id),
+        );
+      } else {
+        return setMyPlayers(currentPlayers => [...currentPlayers, player]);
+      }
+    },
+    [myPlayers],
+  );
+
+  const isSelected = myPlayers.some(pl => pl.id === player.id);
+
   return (
-    <View style={styles.container}>
+    <Pressable
+      onPress={() => handlePlayerPress(player)}
+      style={[
+        styles.container,
+        {backgroundColor: isSelected ? COLORS.purple : COLORS.white},
+      ]}>
       <Image
         source={{
           uri: `https://media.api-sports.io/football/players/${player.id}.png`,
@@ -32,8 +58,8 @@ const PlayerListItem = ({player}: Props) => {
       </View>
 
       <Text style={styles.points}>{player.totalPoints}</Text>
-    </View>
+    </Pressable>
   );
 };
 
-export default PlayerListItem;
+export default memo(PlayerListItem);
